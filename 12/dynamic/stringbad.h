dec5b2ecd96e17a6b6f34417a25aec5ffd715b7f
hive4bee <hive4bee@gmail.com>
2021-06-28 01:02:58 +0900

    동적메모리

---
+++ 12/dynamic/stringbad.h
@@ -0,0 +1,24 @@
+//
+// Created by seongjin on 21. 6. 20..
+//
+
+#ifndef STRINGBAD_H_
+#define STRINGBAD_H_
+
+class StringBad {
+private:
+    char *str;
+    int len;
+    static int num_strings;
+public:
+    StringBad(const char *s);
+
+    StringBad();
+
+    ~StringBad();
+
+    friend std::ostream &operator<<(std::ostream &os, const StringBad &st);
+};
+
+
+#endif //STRINGBAD_H_
